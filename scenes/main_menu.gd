87301813aa41913dde2f91f85d7ad0860294a453
iamcatkin2633 <iamcatkin@foxmail.com>
2026-09-05 23:28:59 +0800

    新版主界面与占位主视觉图

---
+++ scenes/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://hex_game.tscn")
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+
+
+func _on_quit_pressed() -> void:
+	pass
